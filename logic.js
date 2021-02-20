b7596030326ed1ca00cd6f4d390bb8356c3f20c5
Chris Curry <Chamilitary216@hotmail.com>
2021-02-20 18:43:32 -0500

    setup dependencies

---
+++ logic.js
@@ -0,0 +1,13 @@
+const inquirer = require ("inquirer");
+const mysql = require ("mysql");
+const consoleTable = require ("console.table");
+
+const connection = mysql.createConnection ({
+    host: "localhost",
+    port: 3306,
+    password: "root",
+    database: "employee_trackerDB"
+
+}),
+
+
