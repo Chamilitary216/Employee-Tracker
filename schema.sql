96edbdfe4738a5d9c28e23445019c166d6105e2b
Chris Curry <Chamilitary216@hotmail.com>
2021-02-24 01:45:31 -0500

    Got data base tables working in MySQL Workbench

--- schema.sql
+++ schema.sql
@@ -4,30 +4,56 @@ CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
---TABLE
-CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY,
-  name VARCHAR(30),
-);
-
--- Department Table --
-
+-- DEPARTMENT TABLE ----
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30),
+  name VARCHAR(30)
  
 );
-
--- Role Table --
-
-CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR (30),
-  department_id INT
+-- DEPARTMENT TABLE ----
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
   salary DECIMAL,
-  PRIMARY KEY (id)
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
+-- EMPLOYEE ROLE TABLE ----
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+
+);
+
+-- DEPARTMENT SEEDS -----
+INSERT INTO department (name)
+VALUE ("Human Resources");
+INSERT INTO department (name)
+VALUE ("Customer Service");
+INSERT INTO department (name)
+VALUE ("Management");
+
+
+-- EMPLOYEE ROLE SEEDS -------
+INSERT INTO role (title, salary, department_id)
+VALUE ("Fry Cook", 30000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Cashier", 25000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("General Manager", 65000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Server", 10000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Hostess", 30000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Owner", 150000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("HR", 80000, 1);
 
 -- EMPLOYEE SEEDS -------
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
@@ -39,48 +65,13 @@ VALUE ("Patrick","Star",null,3);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
 VALUE ("Squidward", "Tentacles", 1, 4);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Pearl", "Crabs", 4, 5);
+VALUE ("Pearl", "Krabs", 4, 5);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
 VALUE ("Mermaid", "Man", 1, 6);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
 VALUE ("Barnacle", "Boy", 2, 7);
 
--- EMPLOYEE ROLE SEEDS -------
-INSERT INTO role (title, salary, department_id)
-VALUE ("Fry Cook", 150000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUE ("Cashier", 250000, 4);
-INSERT INTO role (title, salary, department_id)
-VALUE ("General Manager", 125000, 3);
-INSERT INTO role (title, salary, department_id)
-VALUE ("Server", 100000, 1);
-INSERT INTO role (title, salary, department_id)
-VALUE ("Hostess", 80000, 1);
-INSERT INTO role (title, salary, department_id)
-VALUE ("Onwer", 120000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUE ("HR", 190000, 4);
 
+SELECT * FROM department;
 SELECT * FROM role;
 SELECT * FROM employee;
-SELECT * FROM department; 
-
-
-
-  * **id** - INT PRIMARY KEY
-  * **name** - VARCHAR(30) to hold department name
-
-* **role**:
-
-  * **id** - INT PRIMARY KEY
-  * **title** -  VARCHAR(30) to hold role title
-  * **salary** -  DECIMAL to hold role salary
-  * **department_id** -  INT to hold reference to department role belongs to
-
-* **employee**:
-
-  * **id** - INT PRIMARY KEY
-  * **first_name** - VARCHAR(30) to hold employee first name
-  * **last_name** - VARCHAR(30) to hold employee last name
-  * **role_id** - INT to hold reference to role employee has
-  * **manager_id** - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
